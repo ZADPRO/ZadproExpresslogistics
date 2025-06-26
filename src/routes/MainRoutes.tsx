4d69f6619417f5a367e0c548ffc5ca41b5755a59
ionhax-dev <thirukumara.d@zadroit.com>
2025-06-26 18:04:07 +0530

    Header file included

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 const MainRoutes: React.FC = () => {
-    return (
-        <div>
-
-        </div>
-    );
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 };
 
 export default MainRoutes;
