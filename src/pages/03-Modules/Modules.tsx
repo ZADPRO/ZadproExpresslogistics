b3d65d66efbdd24714762b8250e050495d895b42
ionhax-dev <thirukumara.d@zadroit.com>
2025-06-27 11:45:49 +0530

    Pages added

---
+++ src/pages/03-Modules/Modules.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Modules: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Modules;
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import Home from "../pages/01-Home/Home";
+import About from "../pages/02-About/About";
+import Modules from "../pages/03-Modules/Modules";
+import Blogs from "../pages/04-Blogs/Blogs";
+import Contact from "../pages/05-Contact/Contact";
 
 const MainRoutes: React.FC = () => {
   return (
@@ -10,7 +15,11 @@ const MainRoutes: React.FC = () => {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/modules" element={<Modules />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
