7eb35557247d5c19956a73e7087e4d34e8eaadb6
ionhax-dev <thirukumara.d@zadroit.com>
2025-06-28 15:35:39 +0530

    Header page drafted

--- src/pages/10-AdminHeader/AdminHeader.tsx
+++ src/pages/10-AdminHeader/AdminHeader.tsx
@@ -12,35 +12,31 @@ const AdminHeader: React.FC = () => {
     <div className="h-screen w-[70px] bg-[#090a58] flex flex-col justify-between items-center py-6 text-white fixed left-0 top-0 z-50">
       {/* Top - Menu Icon */}
       <div>
-        <div className="group relative mb-6 cursor-pointer">
+        <div className="flex flex-col gap-2 items-center">
           <Menu size={24} />
-        </div>
-      </div>
+          <div
+            onClick={() => navigate("/admin/auth/home")}
+            className={`group relative cursor-pointer p-3 mt-3 rounded ${
+              isActive("/admin/auth/home") ? "bg-white text-[#090a58]" : ""
+            }`}
+          >
+            <House size={24} />
+            <span className="absolute left-full top-1/2 -translate-y-1/2 ml-2 hidden group-hover:block bg-[#fca000] text-white font-bold text-lg px-4 py-2 rounded shadow">
+              Home
+            </span>
+          </div>
 
-      {/* Middle - Navigation Icons */}
-      <div className="flex flex-col gap-6 items-center">
-        <div
-          onClick={() => navigate("/admin/auth/home")}
-          className={`group relative cursor-pointer p-1 rounded ${
-            isActive("/admin/auth/home") ? "bg-white text-[#090a58]" : ""
-          }`}
-        >
-          <House size={24} />
-          <span className="absolute left-full top-1/2 -translate-y-1/2 ml-2 hidden group-hover:block bg-[#fca000] text-white font-bold text-lg px-4 py-2 rounded shadow">
-            Home
-          </span>
-        </div>
-
-        <div
-          onClick={() => navigate("/admin/auth/blogPage")}
-          className={`group relative cursor-pointer p-1 rounded ${
-            isActive("/admin/auth/blogPage") ? "bg-white text-[#090a58]" : ""
-          }`}
-        >
-          <NotepadText size={24} />
-          <span className="absolute left-full top-1/2 -translate-y-1/2 ml-2 hidden group-hover:block bg-[#fca000] text-white font-bold text-lg px-4 py-2 rounded shadow">
-            Blogs
-          </span>
+          <div
+            onClick={() => navigate("/admin/auth/blogPage")}
+            className={`group relative cursor-pointer p-3 rounded ${
+              isActive("/admin/auth/blogPage") ? "bg-white text-[#090a58]" : ""
+            }`}
+          >
+            <NotepadText size={24} />
+            <span className="absolute left-full top-1/2 -translate-y-1/2 ml-2 hidden group-hover:block bg-[#fca000] text-white font-bold text-lg px-4 py-2 rounded shadow">
+              Blogs
+            </span>
+          </div>
         </div>
       </div>
 
